a54719a6a33c70102119b3283eee2aaa631f6c8f
Dhruthika11 <143334186+Dhruthika11@users.noreply.github.com>
2025-08-30 19:51:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+
+
+def sayHi(Name): return f"Hi,{Name}."   
+
+def add(x ,y): return x+ y     
+
+def Algebra(X,Y): return (X*Y)*(X+Y)*(X*X)  
+
+def saybye(Name): 
+     return f"Bye,{Name}."   
+
+def subtract(x , y): return x-y  
+
+def Greater(x,y):
+  return x> y    
+
+def lesser(x ,y): return x <y   
+
+print( "Iam writing this code for the second task of the assignment." ) 
+print(sayHi("Dhruthika"))
+print(add(4 ,13))
+print(Algebra(11 , 2))
+print(add(14 , 3))
+print(Algebra(10 ,4))
+print(add(14 ,-10))
+print(Algebra(3,5))
+print(add(7,3))
+print(Algebra(8 ,2))
+print(Greater(14 ,-10))
+print(Greater(3,5))
+print(Greater(7 ,3))
+print(Greater(8,2))
+print(lesser(14 , -10))
+print(lesser(3, 5))
+print(lesser(7,3))
+print(lesser(8,2))
+print(saybye("Dhruthika"))
+
+
